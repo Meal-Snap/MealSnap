2ecd5770d97ed3915260796b77fd71c52837e252
Farhene Sultana <farhenesultana@gmail.com>
2021-04-01 10:24:51 -0400

    Initial Commit

---
+++ Meal Snap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meal Snap
+//
+//  Created by Farhene Sultana on 4/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
